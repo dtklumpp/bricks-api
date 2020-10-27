0e7f6c1ca2ac02ab8fc0bffd372e86ce314b1969
dtklumpp <dtklumpp@gmail.com>
2020-10-27 15:12:14 -0400

    finished basic setup

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=250, null=True, blank=True)
+    description = models.CharField(max_length=2500, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=250)
+
+    projects = models.ManyToManyField(Project)
+
+    def __str__(self):
+        return self.name
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from .models import Category
 
 # Create your views here.
 
