9ebfea890aa57391d9e3e39cb8a4bfee1094d781
dtklumpp <dtklumpp@gmail.com>
2020-11-02 00:51:56 -0500

    got django talking to react with jsons

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('homer/', views.homer, name='homer'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,17 @@ from django.http import HttpResponse, JsonResponse
 from .models import Project
 from .models import Category
 
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 def home(request):
     return JsonResponse({"reply": "hiya"})
     # return HttpResponse('<h1>Hiya!</h1>')
+
+@csrf_exempt
+def homer(request):
+    import json
+    pull_var = json.loads(request.body).get("message")
+    print("message: "+pull_var)
+    return JsonResponse({"response": pull_var})
